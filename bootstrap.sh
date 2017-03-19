c97dd8c7b8c8235e68183fd66fea015b4633b2b9
Austin Chungath Vincent <austincv@gmail.com>
2017-03-19 12:46:47 +0000

    fix problems with interactive console popping up sometimes

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,7 +39,9 @@ echo "
     Updating system packages ...
 "
 sudo apt-get update
-sudo apt-get -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y \
+ -o Dpkg::Options::="--force-confdef" \
+ -o Dpkg::Options::="--force-confold" dist-upgrade
 
 echo 'Start : '${START_TIME}
 echo 'End   : '$(date)
